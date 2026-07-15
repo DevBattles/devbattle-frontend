6b88bff4dd978c49e45ca59a74beb8d530530c48
Vinay-Partap <bhardwajvinay400@gmail.com>
2026-07-15 10:09:20 +0530

    feat: configure routing and protected navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import App from "./App";
 import "./styles/global.css";
 
+import { AuthProvider } from "./context/AuthContext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+            style: {
+              background: "#111827",
+              color: "#fff",
+              border: "1px solid #334155",
+            },
+          }}
+        />
+
+        <App />
+
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
